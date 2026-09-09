5da2685f56c3f856ae7c095ec03abd9f0fd297d4
LHPH <luislyly@hotmail.com>
2026-09-08 20:42:07 -0600

    Add shell for kyc-batch-business

---
+++ shells/kyc-batch-business.sh
@@ -0,0 +1,1 @@
+java -Dspring.profiles.active=prod -Dspring.config.additional-location=/opt/kyc/kyc-config-files/,/opt/kyc/apps/kyc-batch-business/ -jar /opt/kyc/apps/kyc-batch-business/kyc-batch-business.jar --job=$1
